bfcec65497013a5566bcbbbe3f151c855fec1fea
Leunamz <eliudniebles@gmail.com>
2024-03-17 10:06:37 -0500

    se  configura tailwind-color

--- lua/plugins/tailwind-color.lua
+++ lua/plugins/tailwind-color.lua
@@ -1,6 +1,17 @@
 return {
   "themaxmarchuk/tailwindcss-colors.nvim",
-  config = function ()
-    require("colorizer.tailwind_colors").setup()
+  config = function()
+    require("tailwindcss-colors").setup()
+    local nvim_lsp = require("lspconfig")
+
+    local on_attach = function(client, bufnr)
+      -- other stuff --
+      require("tailwindcss-colors").buf_attach(bufnr)
+    end
+
+    nvim_lsp["tailwindcss"].setup({
+      -- other settings --
+      on_attach = on_attach,
+    })
   end
 }
